98589ac7eb5dfb2c9657f97575a5ba5c57ae98c1
johnny <jhuang007007@gmail.com>
2022-03-01 17:20:58 -0500

    draw function fills grid on mouse hover, clearGrid function clears the grid

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+//get number of squares for each side from user
+
+
 //create 16x16 grid of square divs inside the .container div
 function createGrid() {
   let squares = 16;
@@ -8,4 +11,28 @@ function createGrid() {
   }
 }
 
+//allows user to draw on in the grid of divs
+function draw() {
+  const divs = document.querySelectorAll(".container > div");
+  divs.forEach((div) => {
+    div.addEventListener('mouseover', () => {
+      div.setAttribute('style', 'background-color:black')
+    });
+  });
+}
+
+//clear grid when button
+function clearGrid () {
+  const clear = document.querySelector("#clear");
+  const divs = document.querySelectorAll(".container > div");
+  clear.addEventListener('click', () => {
+    divs.forEach((div) => {
+      div.setAttribute('style', 'background-color: white');
+    });
+  })
+}
+
+//call functions
 createGrid();
+draw();
+clearGrid(); 
